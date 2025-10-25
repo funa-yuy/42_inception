37fb5d26bc83f92a4d8e1a0c8df0ff10a4f187c8
funa <orz.home35@gmail.com>
2025-10-25 22:11:09 +0900

    🚧 mariadbできそう

---
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "=== MariaDB Entrypoint Start ==="
+
+DATADIR="/var/lib/mysql"
+SOCKET="/run/mysqld/mysqld.sock"
+
+if [ ! -d "$DATADIR/mysql" ]; then
+	echo "Initializing MariaDB database..."
+
+	# データベース初期化
+	mysqld --initialize-insecure --user=mysql --datadir="$DATADIR"
+
+	echo "Database initialized."
+
+	# 一時的にmysqldを起動
+	mysqld --user=mysql --skip-networking --socket="$SOCKET" &
+	pid="$!"
+
+	# サーバが起動するまで待つ
+	sleep 5
+
+	 # データベースとユーザーを作成
+	mysql --socket="$SOCKET" -u root <<-EOSQL
+			ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+			CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;
+			CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+			GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%';
+			FLUSH PRIVILEGES;
+	EOSQL
+
+	# 一時サーバーを停止
+	mysqladmin --socket="$SOCKET" -u root -p"${MARIADB_ROOT_PASSWORD}" shutdown
+	wait "$pid"
+else
+    echo "Database already initialized, skipping initialization."
+
+fi
+
+echo "=== Starting MariaDB ==="
+
+exec mysqld --user=mysql --datadir="$DATADIR" --socket="$SOCKET"
